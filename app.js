3a96bb820a77d1c2f09490eafb31e6c7536a8e44
GyanendraMani <gyanendramanimishra@gmail.com>
2023-07-02 22:41:50 +0530

    added routes

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+// const exphbs = require('express-handlebars')
+
+const { engine } = require('express-handlebars');
+const home = require('./routes/home/index');
+const admin = require('./routes/admin/index');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-    res.send('it works');
-})
+// app.engine('handlebars', exphbs({defaultLayout: 'home'}))
+// app.set('view engine', 'handlebars');
 
-app.listen(3333, ()=>{
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+app.use('/', home);
+app.use('/admin', admin);
+
+app.listen(3333, () => {
     console.log(`listening on port 3333`)
 })
